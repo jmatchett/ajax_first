7de9b286cb509bc2f5738a0d6a4ec9b2d75350f3
Robby Ranshous <rranshous@gmail.com>
2015-03-02 18:32:56 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AjaxFirst::Application.config.secret_key_base = '9f4902fed7dff47bd73355df61b5f70bc812736aaf33c3e0831ba23764e997a0cd4a4e7fcefe6d23290e38a522850d3bdf790ee09af9571ba8be33a7ec856d52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AjaxFirst::Application.config.session_store :cookie_store, key: '_ajax_first_session'
